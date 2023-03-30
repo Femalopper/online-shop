99c09c7d7f299f4549d65e523a7170582cf45f96
Femalopper <lina-kursk@mail.ru>
2023-03-30 14:56:27 +0300

    refactoring

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -7,45 +7,24 @@ const Contacts = () => {
       <h1>Контакты</h1>
       <div className="about_us">
         <h2>Отдел розничных продаж:</h2>
-        <br />
-        +7 495 790-99-75 с 10 00 до 20 00 (по Московскому времени)
-        <br />
-        <br />
-        +7 903 790-99-75 с 10 00 до 20 00 (по Московскому времени) - WhatsApp \ Viber
-        <br />
-        <br />
-        e-mail: info@Shellac-butik.ru
-        <br />
-        <br />
-        Мы в Инстаграмм: @Nail_mania.ru
-        <br />
-        <br />
-        Мы в Вконтакте: Shellac-butik.ru
-        <br />
-        <br />
-        <br />
+        <p className="p-margin">+7 495 790-99-75 с 10 00 до 20 00 (по Московскому времени)</p>
+        <p className="p-margin">
+          +7 903 790-99-75 с 10 00 до 20 00 (по Московскому времени) - WhatsApp \ Viber
+        </p>
+        <p className="p-margin">e-mail: info@Shellac-butik.ru</p>
+        <p className="p-margin">Мы в Инстаграмм: @Nail_mania.ru</p>
+        <p className="p-margin">Мы в Вконтакте: Shellac-butik.ru</p>
         <h2>Отдел оптовых продаж:</h2>
-        <br />
-        e-mail: info@nail-mania.ru
-        <br />
-        <br />
-        <br />
+        <p className="p-margin">e-mail: info@nail-mania.ru</p>
         <h2>Мастер консультант:</h2>
-        <br />
-        +7 903 790-99-75 c 10 00 до 20 00 (по Московскому времени)
-        <br />
-        <br />
-        В интернет магазине Shellac-butik работает дипломированный мастер маникюра и педикюра,
-        который всегда с радостью ответит на все Ваши вопросы касательно технологии использования
-        товаров нашего магазина
-        <br />
-        <br />
-        e-mail: info@Shellac-butik.ru
-        <br />
-        <br />
-        Юридический адрес: г. Москва, ул. Новая Басманная, д. 15
-        <br />
-        <br />
+        <p className="p-margin">+7 903 790-99-75 c 10 00 до 20 00 (по Московскому времени)</p>
+        <p className="p-margin">
+          В интернет магазине Shellac-butik работает дипломированный мастер маникюра и педикюра,
+          который всегда с радостью ответит на все Ваши вопросы касательно технологии использования
+          товаров нашего магазина
+        </p>
+        <p className="p-margin">e-mail: info@Shellac-butik.ru</p>
+        <p className="p-margin">Юридический адрес: г. Москва, ул. Новая Басманная, д. 15</p>
       </div>
     </section>
   );
